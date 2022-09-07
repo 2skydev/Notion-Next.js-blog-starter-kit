52aef03929c1d1fe24fe97e05cdedd1d6cb90f2d
2skydev <2skydev@gmail.com>
2022-09-07 17:07:45 +0900

    다크모드 버그 수정

--- lib/use-dark-mode.ts
+++ lib/use-dark-mode.ts
@@ -1,10 +1,38 @@
-import useDarkModeImpl from '@fisch0920/use-dark-mode';
+import { useEffect, useState } from 'react';
+
+const isSSR = typeof window === 'undefined';
+
+const isCurrentUserDarkMode = isSSR
+  ? false
+  : window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 export function useDarkMode() {
-  const darkMode = useDarkModeImpl(true, { classNameDark: 'dark-mode' });
+  const storageValue = isSSR ? false : localStorage.getItem('darkMode');
+
+  const [isDarkMode, setIsDarkMode] = useState(
+    storageValue ? storageValue === 'true' : isCurrentUserDarkMode ? true : false,
+  );
+
+  useEffect(() => {
+    const darkModeChangeListener = event => {
+      setIsDarkMode(event.matches);
+    };
+
+    localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', darkModeChangeListener);
+
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', darkModeChangeListener);
+    };
+  }, [isDarkMode]);
 
   return {
-    isDarkMode: darkMode.value,
-    toggleDarkMode: darkMode.toggle,
+    isDarkMode: isDarkMode,
+    toggleDarkMode: () => setIsDarkMode(!isDarkMode),
   };
 }
