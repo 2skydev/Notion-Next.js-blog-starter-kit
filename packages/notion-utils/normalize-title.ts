e73edec65ab5ad6f5a66516830fcf57078e469dd
2skydev <2skydev@gmail.com>
2022-09-07 18:05:16 +0900

    커스텀 주석 표시

--- packages/notion-utils/normalize-title.ts
+++ packages/notion-utils/normalize-title.ts
@@ -1,10 +1,13 @@
 export const normalizeTitle = (title?: string | null): string => {
-  return (title || '')
-    .replace(/ /g, '-')
-    .replace(/[^a-zA-Z0-9-\u4e00-\u9fa5ㄱ-힣]/g, '')
-    .replace(/--/g, '-')
-    .replace(/-$/, '')
-    .replace(/^-/, '')
-    .trim()
-    .toLowerCase();
+  return (
+    (title || '')
+      .replace(/ /g, '-')
+      // CUSTOM: 한국어도 slug 사용 가능하도록 수정
+      .replace(/[^a-zA-Z0-9-\u4e00-\u9fa5ㄱ-힣]/g, '')
+      .replace(/--/g, '-')
+      .replace(/-$/, '')
+      .replace(/^-/, '')
+      .trim()
+      .toLowerCase()
+  );
 };
