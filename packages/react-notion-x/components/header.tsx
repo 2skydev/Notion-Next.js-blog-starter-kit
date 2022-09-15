f617364a9d08b34d1fcc4898c7efd3389691db49
2skydev <2skydev@gmail.com>
2022-09-15 16:55:52 +0900

    헤더 렌더링 최적화

--- packages/react-notion-x/components/header.tsx
+++ packages/react-notion-x/components/header.tsx
@@ -1,60 +1,60 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import { useHotkeys } from 'react-hotkeys-hook'
-import { getPageBreadcrumbs } from 'notion-utils'
-import * as types from 'notion-types'
+import { useHotkeys } from 'react-hotkeys-hook';
+import { getPageBreadcrumbs } from 'notion-utils';
+import * as types from 'notion-types';
 
-import { useNotionContext } from '../context'
-import { PageIcon } from './page-icon'
-import { SearchIcon } from '../icons/search-icon'
-import { cs } from '../utils'
-import { SearchDialog } from './search-dialog'
-import { SearchNotionFn } from '../types'
+import { useNotionContext } from '../context';
+import { PageIcon } from './page-icon';
+import { SearchIcon } from '../icons/search-icon';
+import { cs } from '../utils';
+import { SearchDialog } from './search-dialog';
+import { SearchNotionFn } from '../types';
 
 export const Header: React.FC<{
-  block: types.CollectionViewPageBlock | types.PageBlock
+  block: types.CollectionViewPageBlock | types.PageBlock;
 }> = ({ block }) => {
   return (
-    <header className='notion-header'>
-      <div className='notion-nav-header'>
+    <header className="notion-header">
+      <div className="notion-nav-header">
         <Breadcrumbs block={block} />
         <Search block={block} />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export const Breadcrumbs: React.FC<{
-  block: types.Block
-  rootOnly?: boolean
+const _Breadcrumbs: React.FC<{
+  block: types.Block;
+  rootOnly?: boolean;
 }> = ({ block, rootOnly = false }) => {
-  const { recordMap, mapPageUrl, components } = useNotionContext()
+  const { recordMap, mapPageUrl, components } = useNotionContext();
 
   const breadcrumbs = React.useMemo(() => {
-    const breadcrumbs = getPageBreadcrumbs(recordMap, block.id)
+    const breadcrumbs = getPageBreadcrumbs(recordMap, block.id);
     if (rootOnly) {
-      return [breadcrumbs[0]].filter(Boolean)
+      return [breadcrumbs[0]].filter(Boolean);
     }
 
-    return breadcrumbs
-  }, [recordMap, block.id, rootOnly])
+    return breadcrumbs;
+  }, [recordMap, block.id, rootOnly]);
 
   return (
-    <div className='breadcrumbs' key='breadcrumbs'>
+    <div className="breadcrumbs" key="breadcrumbs">
       {breadcrumbs.map((breadcrumb, index: number) => {
         if (!breadcrumb) {
-          return null
+          return null;
         }
 
-        const pageLinkProps: any = {}
+        const pageLinkProps: any = {};
         const componentMap = {
-          pageLink: components.PageLink
-        }
+          pageLink: components.PageLink,
+        };
 
         if (breadcrumb.active) {
-          componentMap.pageLink = (props) => <div {...props} />
+          componentMap.pageLink = props => <div {...props} />;
         } else {
-          pageLinkProps.href = mapPageUrl(breadcrumb.pageId)
+          pageLinkProps.href = mapPageUrl(breadcrumb.pageId);
         }
 
         return (
@@ -63,67 +63,63 @@ export const Breadcrumbs: React.FC<{
               className={cs('breadcrumb', breadcrumb.active && 'active')}
               {...pageLinkProps}
             >
-              {breadcrumb.icon && (
-                <PageIcon className='icon' block={breadcrumb.block} />
-              )}
+              {breadcrumb.icon && <PageIcon className="icon" block={breadcrumb.block} />}
 
-              {breadcrumb.title && (
-                <span className='title'>{breadcrumb.title}</span>
-              )}
+              {breadcrumb.title && <span className="title">{breadcrumb.title}</span>}
             </componentMap.pageLink>
 
-            {index < breadcrumbs.length - 1 && (
-              <span className='spacer'>/</span>
-            )}
+            {index < breadcrumbs.length - 1 && <span className="spacer">/</span>}
           </React.Fragment>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
+
+export const Breadcrumbs = React.memo(_Breadcrumbs);
 
 export const Search: React.FC<{
-  block: types.Block
-  search?: SearchNotionFn
-  title?: React.ReactNode
+  block: types.Block;
+  search?: SearchNotionFn;
+  title?: React.ReactNode;
 }> = ({ block, search, title = 'Search' }) => {
-  const { searchNotion, rootPageId } = useNotionContext()
-  const onSearchNotion = search || searchNotion
+  const { searchNotion, rootPageId } = useNotionContext();
+  const onSearchNotion = search || searchNotion;
 
-  const [isSearchOpen, setIsSearchOpen] = React.useState(false)
+  const [isSearchOpen, setIsSearchOpen] = React.useState(false);
   const onOpenSearch = React.useCallback(() => {
-    setIsSearchOpen(true)
-  }, [])
+    setIsSearchOpen(true);
+  }, []);
 
   const onCloseSearch = React.useCallback(() => {
-    setIsSearchOpen(false)
-  }, [])
+    setIsSearchOpen(false);
+  }, []);
 
-  useHotkeys('cmd+p', (event) => {
-    onOpenSearch()
-    event.preventDefault()
-    event.stopPropagation()
-  })
+  useHotkeys('cmd+p', event => {
+    onOpenSearch();
+    event.preventDefault();
+    event.stopPropagation();
+  });
 
-  useHotkeys('cmd+k', (event) => {
-    onOpenSearch()
-    event.preventDefault()
-    event.stopPropagation()
-  })
+  useHotkeys('cmd+k', event => {
+    onOpenSearch();
+    event.preventDefault();
+    event.stopPropagation();
+  });
 
-  const hasSearch = !!onSearchNotion
+  const hasSearch = !!onSearchNotion;
 
   return (
     <>
       {hasSearch && (
         <div
-          role='button'
+          role="button"
           className={cs('breadcrumb', 'button', 'notion-search-button')}
           onClick={onOpenSearch}
         >
-          <SearchIcon className='searchIcon' />
+          <SearchIcon className="searchIcon" />
 
-          {title && <span className='title'>{title}</span>}
+          {title && <span className="title">{title}</span>}
         </div>
       )}
 
@@ -136,5 +132,5 @@ export const Search: React.FC<{
         />
       )}
     </>
-  )
-}
+  );
+};
