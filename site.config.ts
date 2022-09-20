48f9be3ae0b9c4a7d0eb1e513f145950b36cc80b
2skydev <2skydev@gmail.com>
2022-09-20 18:27:04 +0900

    카테고리 페이지 추가

--- site.config.ts
+++ site.config.ts
@@ -51,17 +51,12 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'custom',
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationLinks: [
+    {
+      title: '카테고리',
+      pageId: '36400db511474331b5c1de6918212469',
+    },
+  ],
 
   // -------- custom configs (2skydev) -------------
 
