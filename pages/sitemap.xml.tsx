3fb7bc8b09bb35707f49b71f431fc754181643c0
2skydev <2skydev@gmail.com>
2022-09-26 16:44:24 +0900

    같은 주소 두번 들어가있는 코드 제거

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -31,10 +31,6 @@ const createSitemap = (siteMap: SiteMap) =>
   `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>${host}</loc>
-    </url>
-
-    <url>
       <loc>${host}/</loc>
     </url>
 
