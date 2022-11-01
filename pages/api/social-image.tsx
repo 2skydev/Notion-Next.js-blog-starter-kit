d9ed1f599522d63afd65aa62a3f1b45bf0810718
2skydev <2skydev@gmail.com>
2022-11-01 15:01:21 +0900

    og image 생성 방식 변경 nodejs 버전 14이하 강제와 낮은 생성 속도로 인해 @vercel/og으로 변경

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -1,236 +1,130 @@
 import * as React from 'react';
-import { withOGImage } from 'next-api-og-image';
+import { Client } from '@notionhq/client';
 
-import { getBlockTitle, getPageProperty, parsePageId } from 'notion-utils';
-import { PageBlock } from 'notion-types';
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
+import * as siteConfig from 'lib/config';
 
-import { notion } from 'lib/notion-api';
-import { mapImageUrl } from 'lib/map-image-url';
-import * as config from 'lib/config';
+export const config = {
+  runtime: 'experimental-edge',
+};
 
-/**
- * Social image generation via headless chrome.
- *
- * Note: To debug social images, set `debugInspectHtml` to true and load a social
- * image URL. Instead of returning the rendered image, it will return the raw HTML
- * that would've been passed to puppeteer. This makes it much easier to develop
- * and debug issues locally.
- */
-const debugInspectHtml = false;
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-export default withOGImage<'query', 'id'>({
-  template: {
-    react: async ({ id }) => {
-      const pageId = parsePageId(id);
+export default async function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const { id } = Object.fromEntries(searchParams);
 
-      if (!pageId) {
-        throw new Error('Invalid notion page id');
-      }
+    const { properties, cover }: any = await notion.pages.retrieve({
+      page_id: id,
+    });
 
-      const recordMap = await notion.getPage(pageId);
+    const image = cover.external.url;
+    const title = properties['이름'].title[0].plain_text;
+    const description = properties['설명'].rich_text[0].plain_text;
+    const tags = properties['태그'].multi_select.map((tag: any) => tag.name);
+    const author = siteConfig.author;
+    const authorImage = siteConfig.defaultPageIcon;
+    const publishedAt = properties['작성일'].created_time;
+    const publishedAtString = new Date(publishedAt).toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
 
-      const keys = Object.keys(recordMap?.block || {});
-      const block = recordMap?.block?.[keys[0]]?.value;
-
-      if (!block) {
-        throw new Error('Invalid recordMap for page');
-      }
-
-      const isBlogPost = block.type === 'page' && block.parent_table === 'collection';
-      const title = getBlockTitle(block, recordMap) || config.name;
-      const image = mapImageUrl(
-        getPageProperty<string>('Social Image', block, recordMap) ||
-          (block as PageBlock).format?.page_cover ||
-          config.defaultPageCover,
-        block,
-      );
-
-      const imageCoverPosition =
-        (block as PageBlock).format?.page_cover_position ?? config.defaultPageCoverPosition;
-      const imageObjectPosition = imageCoverPosition
-        ? `center ${(1 - imageCoverPosition) * 100}%`
-        : null;
-
-      // const blockIcon = getBlockIcon(block, recordMap);
-
-      const authorImage = mapImageUrl(
-        // blockIcon && isUrl(blockIcon) ? blockIcon : config.defaultPageIcon,
-        config.defaultPageIcon,
-        block,
-      );
-
-      console.log(authorImage);
-
-      const author = getPageProperty<string>('Author', block, recordMap) || config.author;
-
-      // const socialDescription =
-      //   getPageProperty<string>('Description', block, recordMap) ||
-      //   config.description
-
-      const lastUpdatedTime = getPageProperty<number>('Last Updated', block, recordMap);
-      const publishedTime = getPageProperty<number>('작성일', block, recordMap);
-      const dateUpdated = lastUpdatedTime
-        ? new Date(lastUpdatedTime)
-        : publishedTime
-        ? new Date(publishedTime)
-        : undefined;
-      const date =
-        isBlogPost && dateUpdated
-          ? `${dateUpdated.toLocaleString('en-US', {
-              month: 'long',
-            })} ${dateUpdated.getFullYear()}`
-          : undefined;
-      const detail = date || config.domain;
-
-      return (
-        <html>
-          <head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap"
-              rel="stylesheet"
-            />
-            <style dangerouslySetInnerHTML={{ __html: style }} />
-          </head>
-
-          <body>
-            <div className="container">
-              <div className="horiz">
-                <div className="lhs">
-                  <div className="main">
-                    <h1 className="title">{title}</h1>
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            display: 'flex',
+            padding: '46px',
+            background: '#1F2027',
+            color: 'white',
+            width: '100%',
+            height: '100%',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              flex: 1,
+              height: '100%',
+            }}
+          >
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <h1 style={{ fontSize: '46px', fontWeight: 'bold', paddingTop: '24px' }}>{title}</h1>
+              <p style={{ fontSize: '18px', opacity: 0.8 }}>{description}</p>
+              <div style={{ display: 'flex', fontSize: '18px', opacity: 0.6 }}>
+                {tags.map((tag: string, i) => (
+                  <div
+                    key={tag}
+                    style={{ display: 'flex', marginRight: tags.length === i + 1 ? '' : '10px' }}
+                  >
+                    #{tag}
                   </div>
-
-                  <div className="metadata">
-                    {authorImage && (
-                      <div
-                        className="author-image"
-                        style={{ backgroundImage: `url(${authorImage})` }}
-                      />
-                    )}
-
-                    {(author || detail) && (
-                      <div className="metadata-rhs">
-                        {author && <div className="author">{author}</div>}
-                        {detail && <div className="detail">{detail}</div>}
-                      </div>
-                    )}
-                  </div>
-                </div>
-
-                {image && (
-                  <img
-                    src={image}
-                    className="rhs"
-                    style={{
-                      objectPosition: imageObjectPosition || undefined,
-                    }}
-                  />
-                )}
+                ))}
               </div>
             </div>
-          </body>
-        </html>
-      );
-    },
-  },
-  cacheControl: 'max-age=0, s-maxage=86400, stale-while-revalidate=3600',
-  type: 'jpeg',
-  quality: 75,
-  dev: {
-    inspectHtml: debugInspectHtml,
-  },
-});
 
-const style = `
-:root {
-  --padding: 8vmin;
-}
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                src={authorImage}
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  objectFit: 'cover',
+                  objectPosition: 'center',
+                  borderRadius: '50%',
+                  marginRight: '16px',
+                }}
+              />
 
-* {
-  box-sizing: border-box;
-}
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <div style={{ fontSize: '32px', opacity: 0.8 }}>{author}</div>
+                <div style={{ fontSize: '20px', opacity: 0.8 }}>{publishedAtString}</div>
+              </div>
+            </div>
+          </div>
 
-body {
-  font-family: 'Nanum Gothic', sans-serif;
-  padding: 0;
-  margin: 0;
-}
+          {image && (
+            <img
+              src={image}
+              style={{
+                width: '35%',
+                height: '100%',
+                objectFit: 'cover',
+                objectPosition: 'center',
+                borderRadius: '5px',
+                marginLeft: '36px',
+              }}
+            />
+          )}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (e: any) {
+    console.error(`${e.message}`);
 
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: var(--padding);
-  background: #1F2027;
-  color: #fff;
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
-
-.horiz {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  gap: var(--padding);
-  width: 100%;
-  height: 100%;
-}
-
-.lhs {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.rhs {
-  width: 35%;
-  height: 100%;
-  border-radius: 4px;
-  object-fit: cover;
-}
-
-.title {
-  font-size: 3.2em;
-  line-height: 1.3;
-}
-
-.metadata {
-  color: #A9ACC0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: calc(var(--padding) * 0.7);
-  font-size: 1.5em;
-}
-
-.author {
-  font-size: 1.75em;
-}
-
-.author-image {
-  background-size: cover;
-  width: 20vmin;
-  min-width: 20vmin;
-  max-width: 20vmin;
-  height: 20vmin;
-  min-height: 20vmin;
-  max-height: 20vmin;
-  border-radius: 50%;
-  border: 1.5vmin solid #fff;
-}
-
-.metadata-rhs {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 0.3em;
-}
-
-.detail {
-  overflow-wrap: break-word;
-}
-`;
